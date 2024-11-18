60770629ff21039b18bfbf21cfadd857a8eeb327
rond1 <kenssske@yandex.ru>
2024-11-18 16:23:08 +0300

    first commit

--- forms/category.py
+++ forms/category.py
@@ -5,6 +5,6 @@ from wtforms.validators import DataRequired
 
 class CategoryPostForm(FlaskForm):
     name = StringField('Название категории', validators=[DataRequired()])
-    gender = SelectField('Пол', choices=[(1, 'Мальчики'), (-1, 'Девочки'), (0, 'Все')], coerce=int)
+    gender = SelectField('Пол', choices=[(1, 'Мужчины'), (-1, 'Женщины'), (0, 'Все')], coerce=int)
     groups = SelectMultipleField('Группы', validators=[DataRequired()], coerce=int)
     submit = SubmitField('Создать')
--- server.py
+++ server.py
@@ -19,6 +19,7 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 login_manager = LoginManager()
 login_manager.init_app(app)
 
+
 @login_manager.user_loader
 def load_user(user_id):
     db_sess = db_session.create_session()
